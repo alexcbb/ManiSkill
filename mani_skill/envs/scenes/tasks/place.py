828dddef3c201f19d92e6cde3a4d84cec23abe46
arth-shukla <44527268+arth-shukla@users.noreply.github.com>
2024-07-01 01:30:23 -0700

    max_rew

--- mani_skill/envs/scenes/tasks/place.py
+++ mani_skill/envs/scenes/tasks/place.py
@@ -475,7 +475,7 @@ class PlaceSubtaskTrainEnv(SubtaskTrainEnv):
     def compute_normalized_dense_reward(
         self, obs: Any, action: torch.Tensor, info: Dict
     ):
-        max_reward = 33.0
+        max_reward = 39.0
         return self.compute_dense_reward(obs=obs, action=action, info=info) / max_reward
 
     # -------------------------------------------------------------------------------------------------
